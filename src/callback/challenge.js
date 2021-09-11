4da5121035d3d4bb9a10ea23f5824bef9fc9b303
mmoo55 <g.gerson51@gmail.com>
2021-09-10 22:19:40 -0400

    Obteniendo los datos de la API

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,8 @@
-/* Instanciamos la dependencia */
+/* Instanciamos la dependencia para hacer las peticiones */
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+/* URL de la API para hacer las peticiones */
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
     /* Generamos la referencia hacia el objeto que necesitamos */
@@ -19,9 +22,9 @@ function fetchData(url_api, callback) {
         3: processing request (Si hay alguna descarga o alguna información)
         4: request finished and response is ready (Completado)
         */ 
-        if(xhttp.readyState === 4) {
+        if (xhttp.readyState === 4) {
             /* Validamos el status en el que se encuentra la petición */
-            if(xhttp.status === 200) {
+            if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error ' + url_api + ' status: ' + xhttp.status);
@@ -30,5 +33,20 @@ function fetchData(url_api, callback) {
         }
     }
     xhttp.send();
-
 }
+
+// Primero se busca la lista de personajes
+fetchData(API, function (error1, data1) {
+    if (error1) return console.error(error1);
+    // Luego se busca al primer personaje en este caso Rick y obtiene sus datos
+    fetchData(API + data1.results[0].id, function (error2, data2) {
+        if (error2) return console.error(error2);
+        // Por ultimo consulta la url de "origin" del primer personaje para obtener la dimensión
+        fetchData(data2.origin.url, function (error3, data3) {
+            if (error3) console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
