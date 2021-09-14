9802f27666285feed984c2b807384b9b7f563ffe
mmoo55 <g.gerson51@gmail.com>
2021-09-13 21:59:02 -0400

    Corrigiendo la urlen Peticiones a una API mediante Async await

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -6,7 +6,8 @@ const API = 'https://rickandmortyapi.com/api/character/';
 const anotherFunction = async (url_api) => {
     try {
         const data = await fetchData(url_api)
-        const character = await fetchData(`${API}${data.results[0].id}`);
+        // const character = await fetchData(`${API}${data.results[0].id}`);
+        const character = await fetchData(`${url_api}${data.results[0].id}`);
         const origin = await fetchData(character.origin.url);
 
         console.log(data.info.count);
